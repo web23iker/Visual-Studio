07bb3841f63fe461ed63edbccdf33219d4eb8f70
Usuario <Usuario@DESKTOP-2P1O0CS>
2024-03-04 10:28:43 +0100

    Agregar archivos de proyecto.

---
+++ BANAKA/Controllers/CategoryController.cs
@@ -0,0 +1,22 @@
+﻿
+using BANAKA.Data;
+using BANAKA.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BANAKA.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db) 
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            List<Category> objCategoryList = _db.Categories.ToList();
+
+            return View(objCategoryList);
+        }
+    }
+}
---
+++ BANAKA/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BANAKA.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
